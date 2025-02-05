862745a295c229c0436d6ac6e05d83d4e5097bd3
Maiara Franco <maiarafrancoprudencio@gmail.com>
2025-02-05 20:04:06 -0300

    the first project of the course

---
+++ app.js
@@ -0,0 +1,30 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Desafio Amigo Secreto - primeira etapa processo One
+//Implemente uma função para adicionar amigos 
+
+let nomes = [];
+//Capturar o valor do campo de entrada para obter texto insediro pelo usuário
+function adicionarAmigo(){
+    let adicionarAmigo = document.querySelector ('input').value;
+
+if (adicionarAmigo.trim()==='') {//solicitar o preenchimento do campo 'adicionar amigo' através do comando alert
+    window.alert ('Por favor, digite um nome.');
+} else if (nomes.length >=8) {
+        window.alert ('Você já adicionou o número máximo de amigos');
+    } else {
+        nomes.push (adicionarAmigo)
+        console.log(`Amigo adicionado ${nomes}.`);
+        listaNomes (); // atualização dos nomes inseridos pelo usuário
+       }
+//Limpar o campo de entrada após aidiconar um nome
+document.querySelector('input').value='';
+}
+// Implementação da função para atualizar a lista de amigos
+function listaNomes (){
+ let listaNomes = document.querySelector ('#listaAmigos');
+ listaNomes.innerHTML =''; //limpeza da lista para próximas jogadas
+
+ nomes.forEach(function(nome){
+    listaNomes.innerHTML += `<li>${nome}</li>`;
+ })
+}
