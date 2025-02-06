c7fd25e688fae3e28273c9d2deba14ccf553e050
Maiara Franco <maiarafrancoprudencio@gmail.com>
2025-02-06 18:53:39 -0300

    implementacao do sorteio

--- app.js
+++ app.js
@@ -3,17 +3,17 @@
 //Implemente uma função para adicionar amigos 
 
 let nomes = [];
+
 //Capturar o valor do campo de entrada para obter texto insediro pelo usuário
 function adicionarAmigo(){
     let adicionarAmigo = document.querySelector ('input').value;
 
 if (adicionarAmigo.trim()==='') {//solicitar o preenchimento do campo 'adicionar amigo' através do comando alert
     window.alert ('Por favor, digite um nome.');
-} else if (nomes.length >=8) {
+} else if (nomes.length>=8) {
         window.alert ('Você já adicionou o número máximo de amigos');
     } else {
-        nomes.push (adicionarAmigo)
-        console.log(`Amigo adicionado ${nomes}.`);
+        nomes.push (adicionarAmigo);
         listaNomes (); // atualização dos nomes inseridos pelo usuário
        }
 //Limpar o campo de entrada após aidiconar um nome
@@ -26,5 +26,11 @@ function listaNomes (){
 
  nomes.forEach(function(nome){
     listaNomes.innerHTML += `<li>${nome}</li>`;
- })
+ }) 
 }
+//Escreva função para sortear array
+function sortearAmigo(){
+    const amigoAleatorio = Math.floor(Math.random()*nomes.length);
+    const amigoSorteado = nomes [amigoAleatorio];
+    document.getElementById('resultadoAmigoSecreto').innerHTML =`Você sorteou <strong>${amigoSorteado}</strong>!`;
+    }
